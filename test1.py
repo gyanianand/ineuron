ab0f5ca91ae63cf893ea715d7dedab4941dd4b86
gyandmg <ganand@divisionsinc.com>
2022-07-18 16:27:07 +0530

    first push

---
+++ test1.py
@@ -0,0 +1,16 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://gyan3690:Gyan123@cluster0.dd3sr.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d={
+    "name":"sudh",
+    "email":"su@s.com",
+    "surname":"kumar"
+}
+
+db1=client['mongotest']
+col=db1['test']
+col.insert_one(d)
